7813e469e0237642b5fb4a65c9d90a83a8d0dd7b
Ekobits 11 <Ekobits 11>
2017-07-14 14:04:25 -0700

    updated files

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>This Me</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.mini.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+	<div>
+	<!-- logo at left -->
+		<img src="">
+		<!-- navigation start here float at right -->
+		<nav>
+			<ul>
+				<li><a href="#">T.LIST</a></li>
+				<li><a href="#">W.LIST</a></li>
+				<li><a href="#">M.LIST</a></li>
+				<li><a href="#">C.LIST</a></li>
+			</ul>
+		</nav> <!-- Navigation Ends  -->
+
+		 <a href="" type="button">Sign up</a><!--Sign-up into eko register book -->
+	</div>
+
+	<div>
+		
+	</div>
+</div>
+
+
+<!-- My javaScript start here -->
+<script type="text/javascript" src="js/bootstrap.mini.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+	
+</script>
+</body>
+</html>
